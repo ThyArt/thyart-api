6e5b5b3bf68ca58c5efc04b42d8de9bbd80735b5
dylan <dylan.restelli@epitech.eu>
2018-03-19 15:43:16 +0100

    testing

--- app/Customer.php
+++ app/Customer.php
@@ -4,9 +4,27 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Customer
+ *
+ * @property int $id
+ * @property string $firstname
+ * @property string $lastname
+ * @property string $phone
+ * @property string $email
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer whereFirstname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer whereLastname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Customer extends Model
 {
-
     protected $fillable = [
         'firstname', 'lastname', 'phone', 'email'
     ];
