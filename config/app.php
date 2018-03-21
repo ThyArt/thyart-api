0e7b4c0868de58380ebebcbb50777fed97abcc65
Clément Deseine <clement.deseine@gmail.com>
2018-03-21 11:08:55 +0100

    ide-helper in require-dev

--- config/app.php
+++ config/app.php
@@ -159,7 +159,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
         Propaganistas\LaravelPhone\PhoneServiceProvider::class,
     ],
