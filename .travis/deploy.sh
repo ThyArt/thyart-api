008b7b3f0bfb878bf27a1548930e6681b629271c
Clément Deseine <clement.deseine@gmail.com>
2019-08-01 15:04:27 +0200

    add good rights

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,9 +3,11 @@ then
   rsync -r --delete-after --quiet "$TRAVIS_BUILD_DIR"/ root@staging.api.thyart.fr:/var/www/thyart-api/
   ssh root@staging.api.thyart.fr php /var/www/thyart-api/artisan deploy
   ssh root@staging.api.thyart.fr php /var/www/thyart-api/artisan migrate --env=testing
+  ssh root@staging.api.thyart.fr chown -R www-data:www-data /var/www/thyart-api/
 
 elif [[ "$TRAVIS_BRANCH" == 'master' ]];
 then
   rsync -r --delete-after --quiet "$TRAVIS_BUILD_DIR"/ root@api.thyart.fr:/var/www/thyart-api/
   ssh root@api.thyart.fr php /var/www/thyart-api/artisan deploy
+  ssh root@api.thyart.fr chown -R www-data:www-data /var/www/thyart-api/
 fi
