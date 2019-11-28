a95759aad4aa7ec8ee79f1d38237d0e9c063306d
Clément <clement.deseine@gmail.com>
2019-11-28 18:41:49 +0100

    fix description in migration

--- database/migrations/2019_09_11_074825_create_newsletter_table.php
+++ database/migrations/2019_09_11_074825_create_newsletter_table.php
@@ -17,7 +17,7 @@ class CreateNewsletterTable extends Migration
             $table->increments('id');
             $table->unsignedInteger('gallery_id');
             $table->string('subject');
-            $table->string('description');
+            $table->longText('description');
             $table->timestamps();
 
             $table->foreign('gallery_id')->references('id')->on('galleries')->onDelete('cascade');
