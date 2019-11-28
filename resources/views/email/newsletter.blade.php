d35022b8c2303a89e4622feb7d568e4e5b990cb4
Alex <alexandre.corvino@epitech.eu>
2019-11-28 23:23:11 +0100

    Display firstname of the contact into the newsletter

--- resources/views/email/newsletter.blade.php
+++ resources/views/email/newsletter.blade.php
@@ -8,8 +8,7 @@
            {{ $newsletter->subject }}
 @endsection
 
-@section('body')
-    <h3 text-align="center">Hi, {{ $customer->firstName }} !</h3>
+<h3 text-align="center">Hi, {{ $customer->first_name }} !</h3>
     <br/>
 
     <p> {{ $newsletter->description }}</p><br/>
